61d62de1271f8cd8cf23e23e436fbc5c51ccca42
BayronJDv <bayron.jojoa.dv@gmail.com>
2025-03-31 23:31:43 -0500

    coment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@
 
 import Controlador.Controlador;
 
+/**
+ * Clase principal que inicia la aplicación del laberinto.
+ * Su propósito es servir como punto de entrada al programa,
+ * inicializando el controlador que maneja la lógica del juego
+ * y la interfaz gráfica.
+ * 
+ * Esta clase contiene el método main que:
+ * - Inicia el controlador
+ * - Crea la ventana principal
+ * - Configura el laberinto inicial
+ */
+
 public class Main {
 
     public static void main(String[] args) {
